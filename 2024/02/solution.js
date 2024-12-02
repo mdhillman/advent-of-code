93a2a94d9975a63a3ce72da6f57df3cf1a0f8fef
Michael Hillman <the.michael.hillman@gmail.com>
2024-12-02 19:09:06 +0000

    Complete day 02

--- 2024/02/solution.js
+++ 2024/02/solution.js
@@ -3,41 +3,45 @@ import { promises as fs } from 'fs';
 const INPUT_FILE = './2024/02/input.txt';
 const REPORTS = [];
 
-// Read input file and split into left & right parts
+// Read input file and split into numerical reports
 async function readInputFile() {
     const data = await fs.readFile(INPUT_FILE, { encoding: 'utf-8' });
     const lines = data.split(/\r?\n/);
 
     for (const line of lines) {
         const elements = line.split(/[ ]+/);
-        REPORTS.push(elements);
+        REPORTS.push(elements.map(Number));
     }
 }
 
 // Is the input report safe?
-function isSafe(report) {
-    if(report.length <= 1) return false;
-    let failCount = 0;
+function isReportSafe(report) {
+    if (report.length < 2) return true; 
 
-    for(let i = 1; i < report.length; i++) {
-        const prev = Number(report[i - 1]);
-        const current = Number(report[i]);
-        
-        if(current == prev || Math.abs(current - prev) > 3) {
-            failCount++;
-        }
+    const isIncreasing = report[1] > report[0];
+    const isDecreasing = report[1] < report[0];
 
-        if(i < (report.length - 1)) {
-            const next = Number(report[i + 1]);
-            if(prev < current && current > next) {
-                failCount++;
-            }
-            if(prev > current && current < next) {
-                failCount++;
-            }
-        }
+    if (!isIncreasing && !isDecreasing) return false; 
+
+    for (let i = 1; i < report.length; i++) {
+        const difference = Math.abs(report[i] - report[i - 1]);
+        if (difference < 1 || difference > 3) return false;
+        if (isIncreasing && report[i] <= report[i - 1]) return false;
+        if (isDecreasing && report[i] >= report[i - 1]) return false;
     }
-    return failCount == 0;
+    return true;
+}
+
+// Is the input report safe with damping?
+function isReportSafeWithDamping(report) {
+    for(let i = 0; i < report.length; i++) {
+        var reportCopy = [...report];
+        reportCopy.splice(i, 1);
+
+        const safeNow = isReportSafe(reportCopy);
+        if(safeNow) return true;
+    }
+    return false;
 }
 
 // Solve part one
@@ -45,30 +49,11 @@ function partOne() {
     let safeCount = 0;
 
     for(const report of REPORTS) {
-        let failCount = isSafe(report);
-        safeCount += failCount > 0 ? 1 : 0;
+        const isSafe = isReportSafe(report);
+        safeCount += isSafe ? 1 : 0;
     }
 
     console.log("Number of safe reports is: " + safeCount);
-}
-
-// Is the input report safe?
-function isSafeWithDamping(report) {
-    let increasing = null;
-    let safelyDampedOnce = false;
-
-    for(let i = 1; i < report.length; i++) {
-        const prev = Number(report[i - 1]);
-        const current = Number(report[i]);
-        
-        if(!increasing) {
-            increasing = current > prev;
-        }
-
-        }
-    }
-
-    return true;
 }
 
 // Solve part two
@@ -76,14 +61,20 @@ function partTwo() {
     let safeCount = 0;
 
     for(const report of REPORTS) {
-        let wasSafe = isSafeWithDamping(report);
-        safeCount += wasSafe ? 1 : 0;
+        const isSafe = isReportSafe(report);
+
+        if(isSafe) {
+            safeCount++;
+        } else {
+            const safeWithDamping = isReportSafeWithDamping(report);
+            if(safeWithDamping) safeCount++;
+        }
     }
 
-    console.log("Number of safe reports is: " + safeCount);
+    console.log("Number of safe reports with damping is: " + safeCount);
 }
 
 // Start
 await readInputFile();
-//partOne();
+partOne();
 partTwo();
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of code
 
 Repository of my own solutions to the [Advent of Code](https://adventofcode.com/) challenges, primarily written in Javascript.
+
+Be warned, I'm implementing a lot of these early in the morning (and I am decidely not a morning person); I'm simply aiming to get to the correct answer, code cleanliness, performance, or good use of design patterns aren't being considered at all.
